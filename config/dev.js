6dd5cf2bde2d78bf2b8b661a6711c9eb5d8c38bc
c <7160844@qq.com>
2019-11-29 15:37:09 +0800

    为开发环境和生产环境设置不同的 API 接口

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 // NOTE: H5 端使用 devServer 实现跨域,需要修改 package.json 的运行命令,加入环境变量.
 const isH5 = process.env.CLIENT_ENV === "h5";
-const HOST = "\"https://miniapp.you.163.com\"";
-const HOST_M = "\"https://m.you.163.com\"";
+const HOST = "\"http://localhost:5000\"";
+const HOST_M = "\"http://localhost:5000\"";
 
 module.exports = {
     env: {
--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,9 @@
 const isH5 = process.env.CLIENT_ENV === "h5";
 
-const HOST = "\"https://miniapp.you.163.com\"";
-const HOST_M = "\"https://m.you.163.com\"";
+const HOST = "\"http://www.wxcontainer.applinzi.com\"";
+const HOST_M = "\"http://m.wxcontainer.applinzi.com\"";
 
-// XXX 搭了个 proxy 用于演示 prod 环境的 H5
+// XXX 搭了个 proxy 用于演示 prod 环境的 H5.
 const HOST_H5 = "\"http://jsnewbee.com/taro-yanxuan/api\"";
 const HOST_M_H5 = "\"http://jsnewbee.com/taro-yanxuan/api-m\"";
 
@@ -17,6 +17,6 @@ module.exports = {
     },
     weapp: {},
     h5: {
-        publicPath: "/taro-yanxuan"
+        publicPath: "/wxcontainer"
     }
 };
